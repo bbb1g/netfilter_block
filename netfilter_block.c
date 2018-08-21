22de3f27baf0ae528e9af085ac7d86ee18551110
bbb1g <30920085+bbb1g@users.noreply.github.com>
2018-08-21 15:15:02 +0900

    Update netfilter_block.c

--- netfilter_block.c
+++ netfilter_block.c
@@ -26,7 +26,7 @@ int testFunc(unsigned char* buf,unsigned int size) {
 	   strcmp(methodPtr,"DELETE") && strcmp(methodPtr,"OPTIONS"))return 0;
 	*/
 	for(int i=0;i<size-tcp_header_size-ip->ip_hl*4-15;i++){
-		if(!memcmp(methodPtr+i,"test.gilgil.net",15))return 1;
+		if(!memcmp(methodPtr+i,"sex.com",7))return 1;
 	}
 	return 0;
 }
